62879a28a7cd67c0d452606b3d0c8977e1ed5d54
mauzus <mauzus@c664ec4c-197f-2882-fb2d-2df9d830e721>
2008-10-03 00:01:48 +0000

    new default hotkeys

--- trunk/pcsxrr/Win32/maphkeys.c
+++ trunk/pcsxrr/Win32/maphkeys.c
@@ -53,8 +53,8 @@ struct EMUCMDTABLE EmuCommandTable[]=
 	{ '9',             0,           "Select State 9", },
 	{ VK_F7,           VK_CONTROL,  "Load Current State", },
 	{ VK_F5,           VK_CONTROL,  "Save Current State", },
-	{ 0,               0,           "Start Recording", },
-	{ 0,               0,           "Start Playback", },
+	{ 'N',             VK_CONTROL,  "Start Recording", },
+	{ 'R',             VK_CONTROL,  "Start Playback", },
 	{ 'T',             VK_CONTROL,  "Stop Movie", },
 	{ 'M',             VK_CONTROL,  "Memory Cards", },
 	{ 'C',             VK_CONTROL,  "Cheat Editor", },
@@ -62,7 +62,7 @@ struct EMUCMDTABLE EmuCommandTable[]=
 	{ 'W',             VK_CONTROL,  "RAM Watch", },
 	{ 'G',             VK_CONTROL,  "Configure Graphics", },
 	{ 'S',             VK_CONTROL,  "Configure Sound", },
-	{ 'R',             VK_CONTROL,  "Configure CD-ROM", },
+	{ 'D',             VK_CONTROL,  "Configure CD-ROM", },
 	{ 'P',             VK_CONTROL,  "Configure Controllers", },
 	{ 'X',             VK_CONTROL,  "Configure CPU", }
 };
