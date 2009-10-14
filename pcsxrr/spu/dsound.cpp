2cba5c0e643cf5eea2bc88cdb47fc1459814881b
mgambrell <mgambrell@c664ec4c-197f-2882-fb2d-2df9d830e721>
2009-10-14 19:39:48 +0000

    fix building on some systems

--- pcsxrr/spu/dsound.cpp
+++ pcsxrr/spu/dsound.cpp
@@ -4,7 +4,7 @@
 	Copyright (C) 2008-2009 DeSmuME team
 
     This file is part of DeSmuME
-
+ 
     DeSmuME is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
