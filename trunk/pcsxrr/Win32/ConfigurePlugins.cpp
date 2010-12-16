a558e9b24515dcf80ebbd1be0c34485e2ca8a392
mgambrell@gmail.com <mgambrell@gmail.com@c664ec4c-197f-2882-fb2d-2df9d830e721>
2010-12-16 02:41:58 +0000

    change default pad1 to an input plugin that actually has buttons (i.e. not mouse)

--- trunk/pcsxrr/Win32/ConfigurePlugins.cpp
+++ trunk/pcsxrr/Win32/ConfigurePlugins.cpp
@@ -88,7 +88,7 @@ int LoadConfig()
 	GetPrivateProfileString("Plugins", "GPU", "gpuTASsoft.dll", &Config.Gpu[0], 256, Conf_File);
 	GetPrivateProfileString("Plugins", "SPU", "", &Config.Spu[0], 256, Conf_File);
 	GetPrivateProfileString("Plugins", "CDR", "cdrTASiso.dll", &Config.Cdr[0], 256, Conf_File);
-	GetPrivateProfileString("Plugins", "Pad1", "padDudDIMouse.dll", &Config.Pad1[0], 256, Conf_File);
+	GetPrivateProfileString("Plugins", "Pad1", "padSeguDPP.dll", &Config.Pad1[0], 256, Conf_File);
 	GetPrivateProfileString("Plugins", "Pad2", "padSeguDPP.dll", &Config.Pad2[0], 256, Conf_File);
 	GetPrivateProfileString("Plugins", "MCD1", "", &Config.Mcd1[0], 256, Conf_File);
 	GetPrivateProfileString("Plugins", "MCD2", "", &Config.Mcd2[0], 256, Conf_File);
