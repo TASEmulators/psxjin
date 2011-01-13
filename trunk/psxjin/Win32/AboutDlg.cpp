94f373fb4024b315dabffd4a4b4dc17de07574c8
arukado_san@yahoo.fr <arukado_san@yahoo.fr@c664ec4c-197f-2882-fb2d-2df9d830e721>
2011-01-13 02:24:01 +0000

    useless commit, update the about dialog

--- trunk/psxjin/Win32/AboutDlg.cpp
+++ trunk/psxjin/Win32/AboutDlg.cpp
@@ -28,7 +28,7 @@ LRESULT WINAPI AboutDlgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 			SetWindowText(hDlg, _("About"));
 
 			Button_SetText(GetDlgItem(hDlg, IDOK), _("OK"));
-			Static_SetText(GetDlgItem(hDlg, IDC_PCSX_ABOUT_TEXT), _("PCSX EMU\n"));
+			Static_SetText(GetDlgItem(hDlg, IDC_PCSX_ABOUT_TEXT), _("PSXjin\n"));
 			Static_SetText(GetDlgItem(hDlg, IDC_PCSX_ABOUT_AUTHORS), _(LabelAuthors));
 			Static_SetText(GetDlgItem(hDlg, IDC_PCSX_ABOUT_GREETS), _(LabelGreets));
 			Button_SetText(GetDlgItem(hDlg,IDOK), _("OK"));
