1eabb17b15ae9ef0fde20e168e2bc3930a97ede3
andres.delikat <andres.delikat@c664ec4c-197f-2882-fb2d-2df9d830e721>
2011-02-01 17:41:07 +0000

    More hotkey mappings added to menu items

--- trunk/psxjin/Win32/WndMain.cpp
+++ trunk/psxjin/Win32/WndMain.cpp
@@ -2609,13 +2609,11 @@ void UpdateMenuHotkeys()
 	MakeMenuName(EMUCMD_SAVESTATE8, "Savestate Slot 8", ID_FILE_STATES_SAVE_SLOT8);
 	MakeMenuName(EMUCMD_SAVESTATE9, "Savestate Slot 9", ID_FILE_STATES_SAVE_SLOT9);
 
-	//TODO:
-	//Loadstate & Savestate hotkeys
-	//Start playback
-	//Start record
-	//Start Avi
-	//Stop Avi
-	//New Lua script window
+	MakeMenuName(EMUCMD_STARTPLAYBACK, "Start &Playback...", ID_FILE_RECORD_MOVIE);
+	MakeMenuName(EMUCMD_STARTRECORDING, "Start &Recording...", ID_FILE_REPLAY_MOVIE);
+	MakeMenuName(EMUCMD_STARTAVI, "Record &AVI", ID_START_CAPTURE);
+	MakeMenuName(EMUCMD_STOPAVI, "Stop AVI", ID_END_CAPTURE);
+	MakeMenuName(EMUCMD_LUA_OPEN, "&New Lua Script Window...", ID_LUA_OPEN);
 
 	//Emulator---------------------------------------------------
 	MakeMenuName(EMUCMD_RESET, "&Reset", ID_EMULATOR_RESET);
