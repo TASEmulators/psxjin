1444835441528076675f404d4631272da49752f9
kylethomson@gmail.com <kylethomson@gmail.com@c664ec4c-197f-2882-fb2d-2df9d830e721>
2011-02-02 05:50:27 +0000

    revert changes to plugins.
    I desperately need help with my analog control plugin. Someone please teach me what I am doing wrong. (AnalogControlProc)

--- psxjin/plugins.cpp
+++ psxjin/plugins.cpp
@@ -380,7 +380,7 @@ PadDataS padd1, padd2;
 
 unsigned char _PADstartPoll(PadDataS *pad) {
 	bufc = 0;		
-	printf("Pad Type a. %d b. %d\n", pad->controllerType, Movie.padType1);
+	//printf("Analog Values  %03d %03d %03d %03d\n", pad->rightJoyX, pad->rightJoyY, pad->leftJoyX, pad->leftJoyY);
 	switch (pad->controllerType) {
 		case PSE_PAD_TYPE_MOUSE:
 			mousepar[3] = pad->buttonStatus & 0xff;
