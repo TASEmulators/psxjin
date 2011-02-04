48b816a489d069da25a606e18a09097bc6585c85
kylethomson@gmail.com <kylethomson@gmail.com@c664ec4c-197f-2882-fb2d-2df9d830e721>
2011-02-04 05:37:34 +0000

    Input displays as Cyan for Autohold/autofire.
    Analog Controls display when an analog joystick is in action.

--- psxjin/gpu/menu.h
+++ psxjin/gpu/menu.h
@@ -27,10 +27,13 @@
 #ifndef _GPU_MENU_H_
 #define _GPU_MENU_H_
 
+#include "PSXCommon.h"
+
 void DisplayText(void);
 void DisplayFrames(void);
 void DisplayLag(void);
-void DisplayInput(void);
+void DisplayInput(short P1, short P2);
+void DisplayAnalog(PadDataS padd1, PadDataS padd2);
 void DisplayRecording(int RecNum, int MaxPlayer);
 void DisplayMovMode(void);
 void CloseMenu(void);
