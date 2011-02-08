7440306dcef37bf9763a6552bc2bcfa6bfaf9d0c
mgambrell <mgambrell@c664ec4c-197f-2882-fb2d-2df9d830e721>
2011-02-08 07:08:21 +0000

    add widget thingy to analog control

--- psxjin/Win32/analog.cpp
+++ psxjin/Win32/analog.cpp
@@ -1,5 +1,5 @@
 /*  PSXjin - Pc Psx Emulator
- *  Copyright (C) 1999-2003  PSXjin Team
+ *  Copyright (C) 1999-2011  PSXjin Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -23,26 +23,33 @@
 #include <string>
 #include "resource.h"
 #include "PSXCommon.h"
+#include <math.h>
+#include <algorithm>
+
+//TODO: Enable Analog control should be check/unchecked in INITDIALOG based on some WndMain bool
+
+//HOWTO: make sure AllowUserInput is set when the user is allowed to edit the analog panels
+//the analog panels will always redraw themselves from Config.PadLeftX etc. just call UpdateAll() to make them get repainted
+//this, of course, applies even while a movie is playing
+//
+//you should probably change the sliders to work the same as in mupen tasinput plugin. i sort of like it better. but i didnt do that now
 
 HWND AnalogControlHWnd = NULL;
-char Temp_Str[1024];
-
-//TODO: Enable Analog conrol should be check/unchecked in INITDIALOG based on some WndMain bool
+static bool AllowUserInput = true;
 
 void UpdatePositionText(HWND hWnd)
 {
-	if (Config.WriteAnalog)		
 	{
-		Config.WriteAnalog = false;
 		SendDlgItemMessage(hWnd,IDC_PAD_LEFTX,TBM_SETPOS,true,Config.PadLeftX);
 		SendDlgItemMessage(hWnd,IDC_PAD_LEFTY,TBM_SETPOS,true,Config.PadLeftY);
 		SendDlgItemMessage(hWnd,IDC_PAD_RIGHTX,TBM_SETPOS,true,Config.PadRightX);
 		SendDlgItemMessage(hWnd,IDC_PAD_RIGHTY,TBM_SETPOS,true,Config.PadRightY);
 	}
-	sprintf(Temp_Str,"X=%03d Y=%03d",SendDlgItemMessage(hWnd,IDC_PAD_LEFTX,TBM_GETPOS,0,0),SendDlgItemMessage(hWnd,IDC_PAD_LEFTY,TBM_GETPOS,0,0));
-	SetDlgItemText(hWnd, IDC_LeftBox, Temp_Str);
-	sprintf(Temp_Str,"X=%03d Y=%03d",SendDlgItemMessage(hWnd,IDC_PAD_RIGHTX,TBM_GETPOS,0,0),SendDlgItemMessage(hWnd,IDC_PAD_RIGHTY,TBM_GETPOS,0,0));
-	SetDlgItemText(hWnd, IDC_RightBox, Temp_Str);
+	char tmp[1024];
+	sprintf(tmp,"X=%03d Y=%03d",Config.PadLeftX,Config.PadLeftY);
+	SetDlgItemText(hWnd, IDC_LeftBox, tmp);
+	sprintf(tmp,"X=%03d Y=%03d",Config.PadRightX,Config.PadRightY);
+	SetDlgItemText(hWnd, IDC_RightBox, tmp);
 }
 
 void UpdateControls(HWND hWnd)
@@ -55,11 +62,197 @@ void UpdateControls(HWND hWnd)
 	EnableWindow(GetDlgItem(hWnd,IDC_RIGHTGROUP),(Config.enable_extern_analog ? MF_ENABLED:MF_GRAYED) );
 }
 
-void CloseAnalogCongrol(HWND hWnd)
+void CloseAnalogControl(HWND hWnd)
 {
 	Config.enable_extern_analog = false;
 	DestroyWindow(hWnd);
 	AnalogControlHWnd = NULL;
+}
+
+void UpdateAll();
+static WNDPROC wpOrigEditProc;
+
+class AnalogPanel
+{
+public:
+	AnalogPanel(int _which) : which(_which) {}
+	int which;
+	HWND hwnd;
+
+	inline float range() { return 255.9999f; }
+
+	void update()
+	{
+		InvalidateRect(hwnd,NULL,true);
+		UpdateWindow(hwnd);
+	}
+
+	POINT getCurPoint()
+	{
+		int x,y;
+		if(which==0)
+		{
+			x = Config.PadLeftX;
+			y = Config.PadLeftY;
+		}
+		else
+		{
+			x = Config.PadRightX;
+			y = Config.PadRightY;
+		}
+
+		//convert to [0,1]
+		float fx = x/range();
+		float fy = y/range();
+
+		//convert to [0,clientSize]
+		RECT rect;
+		GetClientRect(hwnd,&rect);
+		POINT pt;
+		pt.x = (int)(fx*(rect.right-rect.left));
+		pt.y = (int)(fy*(rect.right-rect.left));
+
+		return pt;
+	}
+
+	//receives pixel coordinates, converts to analog coordinates, and sets emulator state
+	void userSet(int x, int y, bool shift)
+	{
+		RECT rect;
+		GetClientRect(hwnd,&rect);
+
+		//clamp while converting to [0,1]
+		float fx = (float)x/(rect.right-rect.left);
+		float fy = (float)y/(rect.right-rect.left);
+		fx = std::min(std::max(fx,0.0f),1.0f);
+		fy = std::min(std::max(fy,0.0f),1.0f);
+
+		//convert to [-1,1]
+		fx = fx*2-1;
+		fy = fy*2-1;
+
+		//convert to polar coordinates
+		float r = sqrt(fx*fx+fy*fy);
+		float theta = atan2(fy,fx);
+		
+		//optionally the radius can't be more than 1
+		if(shift)
+			r = std::min(r,1.0f);
+
+		//convert back to cartesian [-1,1]
+		fx = r*cos(theta);
+		fy = r*sin(theta);
+
+		//printf("%f, %f\n",fx,fy);
+
+		//yes, some of these conversions are redundant. but they make things clear
+
+		//convert to cartesian integer left[0,255]right and up[0,255]down
+		x = (int)(((fx+1)/2)*range());
+		y = (int)(((fy+1)/2)*range());
+
+		//printf("[%d, %d]\n",x,y);
+		
+		//shove back into emulator state
+		if(which==0)
+		{
+			Config.PadLeftX = x;
+			Config.PadLeftY = y;
+		}
+		else
+		{
+			Config.PadRightX = x;
+			Config.PadRightY = y;
+		}
+
+		UpdateAll();
+	}
+};
+
+static AnalogPanel analogPanels[2] = { AnalogPanel(0), AnalogPanel(1) };
+
+static void UpdateAll()
+{
+	for(int i=0;i<2;i++)
+		analogPanels[i].update();
+	UpdatePositionText(AnalogControlHWnd);
+}
+
+
+//wndproc for the AnalogPanel
+LRESULT APIENTRY AnalogPanelProc(HWND hW, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	AnalogPanel* panel = (AnalogPanel*)GetWindowLong(hW,GWL_USERDATA);
+
+	switch(uMsg)
+	{
+		case WM_LBUTTONDOWN:
+			if(AllowUserInput)
+			{
+				SetCapture(hW);
+				panel->userSet(GET_X_LPARAM(lParam),GET_Y_LPARAM(lParam),wParam&MK_SHIFT);
+			}
+			break;
+
+		case WM_MOUSEMOVE:
+			if(GetCapture()==hW)
+				panel->userSet(GET_X_LPARAM(lParam),GET_Y_LPARAM(lParam),wParam&MK_SHIFT);
+			break;
+
+		case WM_LBUTTONUP:
+			ReleaseCapture();
+			break;
+
+		case WM_PAINT:
+		{
+			PAINTSTRUCT ps;
+			HDC hdc = BeginPaint(hW, &ps);
+			RECT rect;
+			GetClientRect(hW,&rect);
+
+			//only paint this control
+			HRGN rectRgn = CreateRectRgn(rect.left,rect.top,rect.right,rect.bottom);
+			SelectClipRgn(hdc,rectRgn);
+
+			//fill the panel
+			DeleteObject(SelectObject(hdc,GetStockObject(LTGRAY_BRUSH)));
+			Rectangle(hdc,rect.left,rect.top,rect.right,rect.bottom);
+
+			//draw the baseline ellipse
+			DeleteObject(SelectObject(hdc,GetStockObject(WHITE_BRUSH)));
+			Ellipse(hdc,rect.left,rect.top,rect.right,rect.bottom);
+
+			HPEN hpenOld, hpenBlue, hpenRed;
+			hpenBlue = CreatePen(PS_SOLID, 3, RGB(0, 0, 255)); // these need to be re-created every time...
+			hpenRed = CreatePen(PS_SOLID, 7, RGB(255, 0, 0));
+			hpenOld = (HPEN)SelectObject(hdc, hpenBlue);
+
+			//draw the blue line
+			MoveToEx(hdc, (rect.left+rect.right)/2, (rect.top+rect.bottom)/2, NULL);
+			POINT pt = panel->getCurPoint();
+			LineTo(hdc, pt.x,pt.y);
+			SelectObject(hdc, hpenOld);
+			DeleteObject(hpenBlue);
+
+			//draw the X/Y axes
+			MoveToEx(hdc, rect.left, (rect.top+rect.bottom)>>1, NULL);
+			LineTo(hdc, rect.right, (rect.top+rect.bottom)>>1);
+			MoveToEx(hdc, (rect.left+rect.right)>>1, rect.top, NULL);
+			LineTo(hdc, (rect.left+rect.right)>>1, rect.bottom);
+
+			//draw the little red dot
+			hpenOld = (HPEN)SelectObject(hdc, hpenRed);
+			MoveToEx(hdc, pt.x,pt.y, NULL);
+			LineTo(hdc, pt.x,pt.y);
+			SelectObject(hdc, hpenOld);
+			DeleteObject(hpenRed);
+
+			DeleteObject(rectRgn);
+			EndPaint(hW,&ps);
+		}
+	}
+
+	return CallWindowProc(wpOrigEditProc, hW, uMsg, wParam, lParam); 
 }
 
 BOOL CALLBACK AnalogControlProc(HWND hW, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -70,12 +263,22 @@ BOOL CALLBACK AnalogControlProc(HWND hW, UINT uMsg, WPARAM wParam, LPARAM lParam
 	switch(uMsg)
 	{
 		case WM_DESTROY:
-		case WM_CLOSE:
-		case WM_QUIT:
-			CloseAnalogCongrol(hW);
 			break;
+
+		case WM_CLOSE:
+			CloseAnalogControl(hW);
+			break;
+
 		case WM_INITDIALOG:				
 			{
+			analogPanels[0].hwnd = GetDlgItem(hW,IDC_ANALOGLEFT);
+			analogPanels[1].hwnd = GetDlgItem(hW,IDC_ANALOGRIGHT);
+			SetWindowLong(analogPanels[0].hwnd,GWL_USERDATA,(LONG)&analogPanels[0]);
+			SetWindowLong(analogPanels[1].hwnd,GWL_USERDATA,(LONG)&analogPanels[1]);
+			wpOrigEditProc = (WNDPROC)SetWindowLong(analogPanels[0].hwnd,GWL_WNDPROC,(LONG)AnalogPanelProc);
+			wpOrigEditProc = (WNDPROC)SetWindowLong(analogPanels[1].hwnd,GWL_WNDPROC,(LONG)AnalogPanelProc);
+
+
 			GetWindowRect(gApp.hWnd, &r);
 			dx1 = (r.right - r.left) / 2;
 			dy1 = (r.bottom - r.top) / 2;
@@ -107,6 +310,8 @@ BOOL CALLBACK AnalogControlProc(HWND hW, UINT uMsg, WPARAM wParam, LPARAM lParam
 			SendDlgItemMessage(hW,IDC_PAD_RIGHTX,TBM_SETPOS,true,128);
 			SendDlgItemMessage(hW,IDC_PAD_RIGHTY,TBM_SETPOS,true,128);
 			Config.enable_extern_analog = true;
+			break;
+
 		case WM_VSCROLL:
 		case WM_HSCROLL:
 			Config.WriteAnalog = false; 
@@ -114,9 +319,13 @@ BOOL CALLBACK AnalogControlProc(HWND hW, UINT uMsg, WPARAM wParam, LPARAM lParam
 			Config.PadLeftY = SendDlgItemMessage(hW,IDC_PAD_LEFTY,TBM_GETPOS,0,0);
 			Config.PadRightY = SendDlgItemMessage(hW,IDC_PAD_RIGHTY,TBM_GETPOS,0,0);
 			Config.PadRightX = SendDlgItemMessage(hW,IDC_PAD_RIGHTX,TBM_GETPOS,0,0);
+			UpdateAll();
+			break;
+
 		case WM_PAINT: 	
 			UpdatePositionText(hW);
 			break;
+
 		case WM_COMMAND:
 			switch(HIWORD(wParam))
 			{
@@ -133,9 +342,10 @@ BOOL CALLBACK AnalogControlProc(HWND hW, UINT uMsg, WPARAM wParam, LPARAM lParam
 			switch(LOWORD(wParam))
 			{
 				case IDOK:
-					CloseAnalogCongrol(hW);
+					CloseAnalogControl(hW);
 					break;
-			}		
+			}
+			break;
 	}
 	return false;
 }
